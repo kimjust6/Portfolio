b4a426588ed7fbddacb69af74e17c91ea807bf41
Justin Kim <mletemps@hotmail.com>
2022-01-29 00:39:56 -0500

    implemented hamburger, but still need to fix sizing issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
+import { useState } from "react";
+
 import "./app.scss";
 import Navbar from "./components/navbar/Navbar";
 import Intro from "./components/intro/Intro";
 import Projects from "./components/projects/Projects";
 import Contact from "./components/contact/Contact";
 import Resume from "./components/resume/Resume";
-import { useState } from "react";
+import Menu from "./components/menu/Menu"
+
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div className="app">
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+        <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       <div className="sections">
         <Intro/>
         <Projects/>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -37,3 +37,4 @@ export default function Navbar ({menuOpen, setMenuOpen}) {
 };
 
 
+
