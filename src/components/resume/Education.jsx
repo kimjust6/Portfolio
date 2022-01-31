e0bf88fd6e3c0978b1313e7b6b0aeb4c31b53c24
Justin Kim <mletemps@hotmail.com>
2022-01-30 23:23:47 -0500

    added education page

--- src/components/resume/Education.jsx
+++ src/components/resume/Education.jsx
@@ -1,11 +1,22 @@
 import "./education.scss";
 
-const education = () => {
+const Education = () => {
   return (
-    <div className="resume" id="resume">
-        Education
+    <div className="education" id="education">
+      
+      <h1>Education</h1>
+      <p className="degree">
+        Honours Bachelor of Technology in Software Development – GPA 3.9
+      </p>
+      <p className="school">
+      Seneca College, Toronto
+      </p>
+      <p className="date">
+        Expected Graduation: August 2023
+      </p>
+      
     </div>
   );
 };
 
-export default education;
+export default Education;
