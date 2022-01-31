f1463f6080651334e9ea00ff8ef1fe263fb34d70
Justin Kim <mletemps@hotmail.com>
2022-01-31 11:31:01 -0500

    commit before adding new icon packs

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+
+import GitHubIcon from '@mui/icons-material/GitHub';
+import HtmlIcon from '@mui/icons-material/Html';
+import CssIcon from '@mui/icons-material/Css';
+
+
 import "./skills.scss";
 
 const Skills = () => {
   return (
-    <div className="skills" id="skills"></div>
+    <div className="skills" id="contact">
+        <h1>Skills</h1>
+        <div className="skillType">
+            <div className="title">
+                <h2>Front End</h2>
+            </div>
+            <div className="content">
+                
+            </div>
+        </div>
+    </div>
   );
 };
 
