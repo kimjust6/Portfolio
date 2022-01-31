01eb44c2dd7e1033d5b3e9b0c243c321f681026a
Justin Kim <mletemps@hotmail.com>
2022-01-30 23:25:09 -0500

    started project section

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -3,7 +3,26 @@ import "./projects.scss";
 const Projects = () => {
   return (
     <div className = "projects" id="projects">
-        Projects
+        <div className="item">
+          <img src="" alt=""></img>
+          <h3>Blockchain Banking Web Application</h3>
+        </div>
+        <div className="item">
+          <img src="" alt=""></img>
+          <h3>Library Management System</h3>
+        </div>
+        <div className="item">
+          <img src="" alt=""></img>
+          <h3>Eventure</h3>
+        </div>
+        <div className="item">
+          <img src="" alt=""></img>
+          <h3>Boulder Dash Clone</h3>
+        </div>
+        <div className="item">
+          <img src="" alt=""></img>
+          <h3>Shell Script</h3>
+        </div>
     </div>
   );
 };
