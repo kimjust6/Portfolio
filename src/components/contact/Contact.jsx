7e3a2ffc49e885b73dbaad5b00acc71f2e2b9df8
Justin Kim <mletemps@hotmail.com>
2022-02-01 17:17:04 -0500

    changed some class tags to className

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -13,10 +13,10 @@ const Contact = () => {
         <div class="linkedin">
         <a href="https://www.linkedin.com/in/justin-kim6/" target="_blank" rel="noreferrer"><LinkedIn></LinkedIn> LinkedIn</a>
         </div>
-        <div class="github">
+        <div className="github">
         <a href="https://github.com/kimjust6/" target="_blank" rel="noreferrer"><GitHub></GitHub> Github</a>
         </div>
-        <div class="email">
+        <div className="email">
           <a href="mailto:kimjust6@gmail.com" target="_blank" rel="noreferrer"><Mail></Mail> Email</a>
         </div>
         </div>
