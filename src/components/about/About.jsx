4653f2ab9d40ba4410c2b881a8abae17cadaf016
Justin Kim <mletemps@hotmail.com>
2022-02-01 22:47:34 -0500

    modified intro

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -11,10 +11,11 @@ const About = () => {
     </div>
     <div className="content">
         <p>
-            My name is Justin Kim and I am a fullstack developer searching for a co-op position for Summer of 2022.
-            I am a quick learner and am always able to think on my feet.
-            I love learning new technologies and developing new applications. 
-            Have a look at what kind of projects I have been working on and feel free to email me if you have any questions.
+            Hello there! My name is Justin Kim and I am a 3rd year student studying Software Development. 
+            I love learning new technologies and developing new applications.  My current goal is to find a co-op position 
+            for the Summer of 2022.   <br/><br/>
+            In addition to programming, I am interested in market trends and financial news.
+            Have a look at what kind of projects I have been working on and feel free to email me at <a href="mailto:kimjust6@gmail.com">kimjust6@gmail.com</a> if you have any questions.
         </p>
     </div>
 </div>
