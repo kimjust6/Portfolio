b76593b2afaab6541c622fbf14224667905f4007
Justin Kim <mletemps@hotmail.com>
2023-08-03 15:26:52 -0400

    installed uuid, framer-motion

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        protocols: ["https"],
+        remotePatterns: [{ hostname: "i.imgur.com" }],
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
