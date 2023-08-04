544e3121d90d368dc88908192dee903547512ff9
Justin Kim <mletemps@hotmail.com>
2023-08-04 03:08:03 -0400

    added animate presence

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
+"use client";
+import { AnimatePresence } from "framer-motion";
+
 export default function Home() {
     return (
-        <main className="">
-            <div className="flex min-h-xl flex-col items-center justify-between p-24 ">
-                Home Page
-            </div>
-
-        </main>
+        <AnimatePresence>
+            <main className="">
+                <div className="flex min-h-xl flex-col items-center justify-between p-24 ">
+                    Home Page
+                </div>
+            </main>
+        </AnimatePresence>
     );
 }
