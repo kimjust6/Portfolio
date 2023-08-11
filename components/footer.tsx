fb157f42ed53f7dc2eec6b5091a739d253482053
Justin Kim <mletemps@hotmail.com>
2023-08-11 02:02:39 -0400

    updated footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer className="w-screen flex justify-center pb-6">
             <a>
-                This website was built with Next.js, React, Tailwind CSS and Framer Motion. The
+                I built this website with Next.js, React, Tailwind, and Framer Motion. The
                 source code is available on my{" "}
                 <a href="https://github.com/kimjust6/Portfolio" className=" text-indigo-600 underline font-semibold">GitHub</a>.
             </a>
