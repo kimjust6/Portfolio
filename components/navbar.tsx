988de68679b49a75ddff1910d0b684772566d215
Justin Kim <mletemps@hotmail.com>
2023-11-29 18:34:54 -0500

    updated links to show full name of link

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,21 +1,32 @@
-"use client";
+'use client';
 
-import { useActiveSection } from "@/app/context/active-section-context";
-import { links } from "@/lib/data";
-import clsx from "clsx";
-import { motion } from "framer-motion";
-import Link from "next/link";
-import ThemeToggle from "./theme-toggle";
+import { useActiveSection } from '@/app/context/active-section-context';
+import { links } from '@/lib/data';
+import clsx from 'clsx';
+import { motion, useMotionValueEvent, useScroll } from 'framer-motion';
+import Link from 'next/link';
+import { useState } from 'react';
+import ThemeToggle from './theme-toggle';
 
 const Navigation = () => {
     const { activeSection, setActiveSection, setTimeOfLastClick } = useActiveSection();
+    const [isOnTop, setIsOnTop] = useState(true);
+    const { scrollY } = useScroll();
+    useMotionValueEvent(scrollY, 'change', (latest) => {
+        if (latest > 150) {
+            setIsOnTop(false);
+        } else {
+            setIsOnTop(true);
+        }
+    });
+
     return (
-        <header className="flex justify-center flex-row w-screen ">
+        <nav className="flex justify-center flex-row w-screen ">
             <div
-                className="z-100 flex justify-center items-center fixed sm:rounded-full bg-amber-200 opacity-95 
+                className={`z-100 flex justify-center items-center fixed bg-amber-200 opacity-95 
                 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-95 sm:bg-opacity-80 shadow-md sm:shadow-lg shadow-gray-300 
-                sm:mt-6 px-4 py-1 sm:py-2 rounded-md border sm:border-amber-300 text-lg z-[999] flex-wrap w-screen sm:w-fit
-                dark:bg-gray-800  dark:border-gray-600 dark:shadow-none"
+                px-4 py-1 rounded-md border sm:border-amber-300 text-lg z-[999] flex-wrap w-screen 
+                dark:bg-gray-800  dark:border-gray-600 dark:shadow-none  sm:py-2 sm:rounded-full sm:mt-6 sm:w-fit`}
             >
                 <motion.span
                     className="sm:hidden mr-4"
@@ -36,9 +47,9 @@ const Navigation = () => {
                                 href={link.hash}
                                 data-text={link.name}
                                 className={clsx(
-                                    "my_navbar text-gray-700 mx-2 text-zinc-9500 opacity-100 dark:text-gray-50 hover:text-gray-950 hover:font-semibold py-1 data-text-title select-none",
+                                    'my_navbar text-gray-700 mx-2 text-zinc-9500 opacity-100 dark:text-gray-50 hover:text-gray-950 hover:font-semibold transition-all py-1 data-text-title select-none',
                                     {
-                                        "font-semibold text-gray-950": activeSection === link.name,
+                                        'font-semibold text-gray-950': activeSection === link.name,
                                     }
                                 )}
                                 onClick={() => {
@@ -53,7 +64,7 @@ const Navigation = () => {
                                     <motion.div
                                         layoutId="pill"
                                         transition={{
-                                            type: "spring",
+                                            type: 'spring',
                                             stiffness: 380,
                                             duration: 1,
                                             damping: 30,
@@ -74,18 +85,7 @@ const Navigation = () => {
                     <ThemeToggle />
                 </motion.span>
             </div>
-            {/* <motion.button
-                className={
-                    "button_primary  fixed z-20 top-16 right-10 hidden  " 
-                    // + (activeSection === "Home" ? "hidden" : "sm:display")
-                }
-                initial={{ y: 100, opacity: 0 }}
-                animate={{ y: 0, opacity: 1 }}
-                transition={{ type: "spring", delay: 0.2, duration: 0.2 }}
-            >
-                Download CV
-            </motion.button> */}
-        </header>
+        </nav>
     );
 };
 
