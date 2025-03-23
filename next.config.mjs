14bf127dc8dc00642f8ec17acc5c801d1b8c164d
JK <mletemps@hotmail.com>
2025-03-23 19:15:54 -0400

    removed cloudflare package

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,15 @@
-import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
+// import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "i.imgur.com",
-            },
-        ],
+        remotePatterns: [{ protocol: "https", hostname: "i.imgur.com" }],
     },
     reactStrictMode: false,
 };
 
 if (process.env.NODE_ENV === "development") {
-    await setupDevPlatform();
+    // await setupDevPlatform();
 }
 
 export default nextConfig;
