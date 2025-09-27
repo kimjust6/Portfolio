cb7bbc95fd695876f3c20db003a4f9f62ab9e877
Justin Kim <mletemps@hotmail.com>
2025-09-27 00:04:38 -0400

    additional minor styling changes

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -23,9 +23,9 @@ const Navigation = () => {
 
     return (
         <nav className="flex w-screen flex-row justify-center">
-            <div className="fixed z-100 flex w-screen flex-wrap items-center justify-center rounded-md border border-amber-300 bg-amber-200/95 bg-clip-padding px-4 py-1 text-lg opacity-95 shadow-md shadow-gray-300 backdrop-blur-lg sm:bg-amber-200/80 sm:shadow-lg lg:mt-6 lg:w-fit lg:py-2 dark:border-gray-600 dark:bg-gray-800 dark:shadow-none">
+            <div className="fixed z-100 flex w-screen flex-wrap items-center justify-center rounded-md border border-amber-300 bg-amber-200/95 bg-clip-padding px-4 py-1 text-lg opacity-95 shadow-md shadow-gray-300 backdrop-blur-lg sm:py-2 lg:mt-6 lg:w-fit lg:bg-amber-200/80 lg:shadow-lg dark:border-gray-600 dark:bg-gray-800 dark:shadow-none">
                 <motion.span
-                    className="mr-4 sm:hidden"
+                    className="mr-4 lg:hidden"
                     initial={{ y: -100, opacity: 0 }}
                     animate={{ y: 0, opacity: 1 }}
                 >
@@ -74,7 +74,7 @@ const Navigation = () => {
                     );
                 })}
                 <motion.span
-                    className="hidden sm:flex"
+                    className="hidden lg:flex"
                     initial={{ y: -100, opacity: 0 }}
                     animate={{ y: 0, opacity: 1 }}
                 >
